79b757eeedf5914d4388761a4594dfdf7906893f
garcia-nacho <garcia.nacho@gmail.com>
2021-03-30 13:52:52 +0200

    30March2021

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use rust_htslib::bam::{IndexedReader, Read, Record};
 use clap::{Arg, App};
 use std::sync::Arc;
+
 //use std::time::Instant;
 
 
@@ -21,37 +22,35 @@ fn main() {
 
 
     let mut bam = IndexedReader::from_path(&myfile).unwrap();
-    //bam.fetch((0, 10500, 10510)).unwrap(); 
+    bam.fetch((0, 10500, 10510)).unwrap(); 
     //MN908947.3
 
     for p in bam.pileup() {
         let pileup = p.unwrap();
-        //println!("{}:{} depth {}", pileup.tid(), pileup.pos(), pileup.depth());
-        let mut seq_string = String::from("Seq:");
-        let mut foo = Vec::new();
+
+        let mut total_base = Vec::new();
 
         for alignment in pileup.alignments() {
             if !alignment.is_del() && !alignment.is_refskip() {
-                //println!("{}", alignment.record().seq()[alignment.qpos().unwrap()] as char);
-                //seq_string.push(alignment.record().seq()[aligSnment.qpos().unwrap()] as char);
+
             
-                foo.push(alignment.record().seq()[alignment.qpos().unwrap()]);
-                //println!("{}", alignment.record().seq());
+                total_base.push(alignment.record().seq()[alignment.qpos().unwrap()]);
+
             }
             else{
-                foo.push(0);
-
-
+                total_base.push(0);
             }
-
-                        
-
+                    
         }
+
         let mut read_vector = Vec::new();
         let mut majority =0;
         let mut majority_base="N";
+        let mut majority_base2="N";
+        let bases = vec!["A", "T", "C","G","D"];
 
-        let base_A = foo.iter().filter(|&n| *n == 65).count();
+        // Base A
+        let base_A = total_base.iter().filter(|&n| *n == 65).count();
         read_vector.push(base_A);
 
         if base_A > majority {
@@ -59,31 +58,36 @@ fn main() {
             majority_base = "A";
         }
 
-        let base_T = foo.iter().filter(|&n| *n == 84).count();
+        // Base T
+        let base_T = total_base.iter().filter(|&n| *n == 84).count();
         read_vector.push(base_T);
 
         if base_T > majority {
             majority=base_T;
-            majority_base = "T";
+            majority_base = "T";    
         }
-        
-        let base_C = foo.iter().filter(|&n| *n == 67).count();
+
+        //Base C
+        let base_C = total_base.iter().filter(|&n| *n == 67).count();
         read_vector.push(base_C);
 
         if base_C > majority {
             majority=base_C;
-            majority_base = "C";
+            majority_base = "C";       
         }
 
-        let base_G = foo.iter().filter(|&n| *n ==71).count();
+
+        //Base G
+        let base_G = total_base.iter().filter(|&n| *n ==71).count();
         read_vector.push(base_G);
 
         if base_G > majority {
             majority=base_G;
             majority_base = "G";
         }
- 
-        let base_D= foo.iter().filter(|&n| *n == 0).count();
+
+        // Deletions
+        let base_D= total_base.iter().filter(|&n| *n == 0).count();
         read_vector.push(base_D);
 
         if base_D > majority {
@@ -91,18 +95,28 @@ fn main() {
             majority_base = "D";
         }
 
+        //Sequence 2 calculation
+        let permutation = permutation::sort(&read_vector[..]);
+        let ordered_bases = permutation.apply_slice(&bases[..]);
+        let ordered_reads = permutation.apply_slice(&read_vector[..]);
+        let mut freq_mr2: f64 = 0.0;
+        if ordered_reads[3]==0 {
+            majority_base2 = ordered_bases[4];
+        }else{
+            majority_base2 = ordered_bases[3];
+            majority2= ordered_reads[3];
+            freq_mr2 = majority2 as f64 / pileup.depth() as f64;
+        }
+        
 
-        //let noise: f32 =  (pileup.depth() as u16 - majority as u16) / pileup.depth() as u16;
+        //Noise calculation
         let noise: f64 =  (pileup.depth() as u16 - majority as u16).into();
         let noise_t= noise / pileup.depth() as f64;
 
-
-
-        println!("{}\t{}\t{}\t{}", pileup.pos(),noise_t,pileup.depth() as u32,majority_base);
+        println!("{}\t{}\t{}\t{}\t{}\t{}", pileup.pos(),noise_t,pileup.depth() as u32,majority_base, majority_base2, freq_mr2);
         //println!("Total {}", pileup.depth() as u16);
-        //println!("Noise {:?}", noise);
-        //println!("Noise_t {:?}", noise_t);
-        //println!("MR {:?}", majority_base);
+
+        //println!("{:?}", ordered_bases);
         
 
     }
